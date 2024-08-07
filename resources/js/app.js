482ce7348055d1995991d4f5ab38e19b1ae5c352
Danny Hobby <danny.hobby@outlook.com>
2024-08-07 11:25:05 +0100

    Passkeys added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,19 @@
-import './bootstrap';
+import { startRegistration } from "@simplewebauthn/browser";
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
+
+document.addEventListener("alpine:init", () => {
+    Alpine.data("registerPasskey", () => ({
+        async register() {
+            const options = await axios.get("/api/passkeys/register");
+            const passkey = await startRegistration(options.data);
+
+            console.log(passkey);
+        },
+    }));
+});
 
 Alpine.start();
